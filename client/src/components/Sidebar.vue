6c252955aabc4841afbeb3c85c0652125716b174
ivan <ivansu.webmail@gmail.com>
2018-09-18 14:20:51 +0700

    show article title, and link it to title

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -1,16 +1,39 @@
 <template>
   <div class="col-lg-3">
     <div class="list-group">
-      <a href="#" class="list-group-item">Post - 1</a>
-      <a href="#" class="list-group-item">Post - 2</a>
-      <a href="#" class="list-group-item">Post - 3</a>
+      <a href="javascript:void(0)" class="list-group-item" v-for="article in articles" @click="getDetail(article._id)"> {{ article.title }} </a>
     </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios';
+
   export default {
-    name: 'Sidebar'
+    name: 'Sidebar',
+    data() {
+      return {
+        articles: []
+      }
+    },
+    methods: {
+      getDetail(id) {
+        this.$emit('get-detail', id);
+      }
+    },
+    created() {
+      axios({
+        method: 'GET',
+        url: 'http://localhost:3001/articles'
+      })
+      .then(response => {
+        let articles = response.data.articles;
+        this.articles = articles;
+      })
+      .catch(err => {
+        console.log(err.response.data);
+      });
+    },
   }
 </script>
 
