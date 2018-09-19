316c8c7ad81a09edc8d6257c0c9584be70b38775
ivan <ivansu.webmail@gmail.com>
2018-09-19 16:35:54 +0700

    delete commented code and add loader

--- client/src/components/AllArticles.vue
+++ client/src/components/AllArticles.vue
@@ -1,25 +1,12 @@
 <template>
   <div class="col-lg-9">
-    <!-- FORM POST
     <div class="row">
-      <div class="col-lg-9 col-sm-9 portfolio-item">
-        <div>
-          <div class="form-group">
-            <input type="text" class="form-control" placeholder="Title...">
-            <br>
-            <input type="text" class="form-control" value="John Doe" disabled>
-            <br>
-            <textarea class="form-control" rows="10" placeholder="Write something..."></textarea>
-          </div>
-          <div class="form-group">
-            <button type="button" class="btn btn-primary">POST</button>
-          </div>
+      <div class="lds-ripple-container">
+        <div class="lds-ripple">
+          <div></div><div></div>
         </div>
       </div>
-    </div>
-    <hr>
-    -->
-    <div class="row">
+
       <div class="col-lg-6 col-sm-6 portfolio-item" v-for="(article, index) in articles" :key="index">
         <div class="card h-70 custom-card">
           <div class="card-body">
@@ -28,6 +15,9 @@
             </h4>
             <p class="translate">
               Author: {{ article.author.name }}
+            </p>
+            <p>
+              <a href="javascript:void(0)">Edit</a> | <a href="javascript:void(0)">Delete</a>
             </p>
           </div>
         </div>
@@ -47,6 +37,7 @@ export default {
     }
   },
   created () {
+
     axios({
       method: 'GET',
       url: `${this.$baseurl}/articles`
@@ -67,6 +58,53 @@ export default {
 
 .custom-card {
   margin: 5px
+}
+
+.lds-ripple-container {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 99;
+}
+
+.lds-ripple {
+  display: inline-block;
+  position: relative;
+  width: 64px;
+  height: 64px;
+  left: 50%;
+  top: 50%;
+  margin-left: -32px;
+  margin-top: -32px;
+}
+.lds-ripple div {
+  background-color: #000000;
+  position: absolute;
+  border: 4px solid #fff;
+  opacity: 1;
+  border-radius: 50%;
+  animation: lds-ripple 1s cubic-bezier(0, 0.2, 0.8, 1) infinite;
+}
+.lds-ripple div:nth-child(2) {
+  animation-delay: -0.5s;
+}
+@keyframes lds-ripple {
+  0% {
+    top: 28px;
+    left: 28px;
+    width: 0;
+    height: 0;
+    opacity: 1;
+  }
+  100% {
+    top: -1px;
+    left: -1px;
+    width: 58px;
+    height: 58px;
+    opacity: 0;
+  }
 }
 
 </style>
