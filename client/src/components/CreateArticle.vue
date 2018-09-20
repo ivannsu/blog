dc612632893e0af1d7aca40df9f6f0d77941e60b
ivan <ivansu.webmail@gmail.com>
2018-09-20 10:59:08 +0700

    create component: CreateArticle

---
+++ client/src/components/CreateArticle.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="col-lg-9">
+    <h1 class="text-center">Create New Article</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CreateArticle'
+}
+
+</script>
+
+<style>
+
+</style>
