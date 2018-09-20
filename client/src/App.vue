2565087afbed36eb9fdb6d9fb29965cd9b6aea73
ivan <ivansu.webmail@gmail.com>
2018-09-20 10:57:00 +0700

    comment vue cli nav style and add loader style

--- client/src/App.vue
+++ client/src/App.vue
@@ -12,6 +12,7 @@
   text-align: center;
   color: #2c3e50;
 }
+/*
 #nav {
   padding: 30px;
 }
@@ -23,5 +24,26 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}*/
+
+.loader-container {
+  position: relative;
+  width: 100%;
+  height: 100%;
+}
+
+.loader {
+  transform: translate(-50%, 50%);
+  border: 6px solid #f3f3f3; /* Light grey */
+  border-top: 6px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 60px;
+  height: 60px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
 }
 </style>
