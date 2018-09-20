9948a2278301df5411b37cf9f686809421778463
ivan <ivansu.webmail@gmail.com>
2018-09-20 10:58:37 +0700

    add dynamic route link

---
+++ client/src/components/Sidebar.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="col-lg-3">
+    <div class="list-group">
+      <router-link to="/" class="list-group-item bg-secondary text-white">Create New Post</router-link>
+    </div>
+    <div class="loader-container" v-if="articles.length === 0 && !empty">
+      <div class="loader"></div>
+    </div>
+    <p v-else-if="articles.length === 0 && empty">No Articles Data</p>
+    <div class="list-group" v-else>
+      <router-link :to="{ name: 'detail-article', params: { articleId: `${article._id}` } }" class="list-group-item" v-for="(article, index) in articles" :key="index">{{ article.title }}</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'Sidebar',
+  data () {
+    return {
+      articles: [],
+      empty: false
+    }
+  },
+  methods: {
+
+  },
+  created () {
+    axios({
+      method: 'GET',
+      url: `${this.$baseurl}/articles`
+    })
+      .then(response => {
+        let articles = response.data.articles
+
+        if (articles.length === 0) {
+          this.empty = true
+        } else {
+          this.articles = articles
+          this.empty = false
+        }
+      })
+      .catch(err => {
+        console.log(err.response.data.message)
+      })
+  }
+}
+
+</script>
