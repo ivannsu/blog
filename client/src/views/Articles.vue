f70f68c2aa7830820777d42de2c180504de98e46
ivan <ivansu.webmail@gmail.com>
2018-09-20 11:00:00 +0700

    refactor and add padding top style

---
+++ client/src/views/Articles.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Navbar/>
+    <div class="container-fluid">
+      <div class="row" style="padding-top: 30px">
+      <Sidebar/>
+      <router-view></router-view>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+
+export default {
+  name: 'Articles',
+  components: {
+    Navbar,
+    Sidebar
+  }
+}
+
+</script>
